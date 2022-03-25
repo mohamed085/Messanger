d2e68a6d3b94a0f56ebc61529f0944f284a2c8c7
Mohamed Hashad <mohamed085555@gmail.com>
2022-03-25 15:18:05 +0200

    solve bug

--- src/main/java/com/messenger/repository/UserRepository.java
+++ src/main/java/com/messenger/repository/UserRepository.java
@@ -17,10 +17,10 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     @Query(value = "SELECT * \n" +
             "FROM user As user\n" +
-            "LEFT JOIN user_requests As requests ON requests.user_id = user.id AND requests.user_id = ?1 \n" +
-            "LEFT JOIN user_requests AS requseted ON requseted.requests_id = user.id AND requseted.user_id = ?1 \n" +
-            "LEFT JOIN user_friends AS friend ON friend.friend_id = user.id AND friend.user_id = ?1 \n" +
-            "WHERE requests.user_id IS null AND requseted.user_id IS NULL AND friend.user_id IS NULL AND user.id <> ?1 ",
+            "LEFT JOIN user_requests As requests ON requests.user_id = user.id AND requests.requests_id = ?1\n" +
+            "LEFT JOIN user_requests AS requseted ON requseted.requests_id = user.id AND requseted.requests_id = ?1\n" +
+            "LEFT JOIN user_friends AS friend ON friend.friend_id = user.id AND friend.user_id = ?1\n" +
+            "WHERE requests.user_id IS null AND requseted.requests_id IS NULL AND friend.user_id IS NULL AND user.id <> ?1",
             countQuery = "SELECT count(*) FROM USER",
             nativeQuery = true)
     List<User> discoverNewUsers(Long id);
